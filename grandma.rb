99fe26de5010ed9ee23518d8cb6a1e761e6acd34
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:07:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-   elseif phrase == phrase.downcase
+   elsif phrase == phrase.downcase
     "H"
   end
 end
