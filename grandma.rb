8100fb7965cfb844d592a9809aaebc2c990abc4b
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:11:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
      "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == ""
+  elsif phrase == "Hi"
   end
 end
 
