4c6981a6f6a59f280b533216ab0ea7d28770e6d4
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:02:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  if phr == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
