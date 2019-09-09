edd1c66020206118f66a459755fb9d62cea50c21
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 18:56:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-   if string == string
-     puts string.upercase
+   
 end
 
 # Whatever you say to grandma, she should respond with
