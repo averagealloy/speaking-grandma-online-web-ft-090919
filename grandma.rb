ebe3a61b8839e585d195b69af83bb8a110d32849
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 18:34:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA"
-    print "i "
+    print "I "
 end
 
 # Whatever you say to grandma, she should respond with
