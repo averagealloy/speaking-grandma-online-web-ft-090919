8a1ae86748b7a0465a2e3876328864532b0f3641
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:13:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
-    "No, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
 
