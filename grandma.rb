9ce7d6549e926e96db1c91be644a13687032c393
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:03:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    retu "I LOVE YOU TOO PUMPKIN!"
+    retun "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
